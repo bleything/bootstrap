deb1857fe6c462451a6fadd7a09604bfddbccba7
Ben Bleything <ben@bleything.net>
2011-12-07 10:02:28 -0800

    add apps cookbook

---
+++ cookbooks/apps/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Ben Bleything"
+maintainer_email "ben@bleything.net"
+license          "Apache 2.0"
+description      "Installs/Configures apps"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+depends 'dmg'
+depends 'mac_os_x'
