e78d787f893d6158a319e778bf7eea057a3c1f09
Ben Bleything <ben@bleything.net>
2011-12-07 09:51:02 -0800

    add Rakefile and rake/solo configs

---
+++ config/solo.rb
@@ -0,0 +1,11 @@
+current_dir = File.expand_path(File.dirname(__FILE__))
+
+file_cache_path  "#{current_dir}/../.chef/cache"
+file_backup_path "#{current_dir}/../.chef/backup"
+cache_options(   :path => "#{current_dir}/../.chef/checksums", :skip_expires => true )
+
+role_path "#{current_dir}/../roles"
+cookbook_path [
+  "#{current_dir}/../cookbooks",
+  "#{current_dir}/../vendored_cookbooks"
+]
