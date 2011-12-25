41869d187f3eb07a4333e20edb6f4bef39e30dbd
Ben Bleything <ben@bleything.net>
2011-12-25 11:59:32 -0800

    add 1password cookbooks and settings

---
+++ cookbooks/apps/recipes/1password.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: apps
+# Recipe:: 1password
+#
+# Copyright 2011, Ben Bleything <ben@bleything.net>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "1password::default"
+
+backup_path = File.expand_path("~/Dropbox/Backups/1Password")
+directory(backup_path) { action :create ; recursive true }
+
+keychain_path = File.expand_path( "~/Dropbox/1Password.agilekeychain" )
+
+settings = {
+  "AgileKeychainLocation"         => keychain_path,
+  "keychainBackupFolderPath"      => backup_path,
+  "AGUpdateAllowBetas"            => true,
+  "AGInstallBrowserExtensions"    => true,
+  "AGAskedAboutBrowserExtensions" => false,
+  "keychainRotationFrequency"     => 10,
+  "License"                       => {
+    "Person" => node['1password'].name,
+    "Serial" => node['1password'].key
+  }
+}
+
+settings.each do |k,v|
+  mac_os_x_userdefaults k do
+    domain "ws.agile.1Password"
+    key    k
+    value  v
+  end
+end
--- roles/osx.rb
+++ roles/osx.rb
@@ -2,6 +2,7 @@ name 'osx'
 description 'osx recipes'
 
 run_list %w(
+  recipe[apps::1password]
   recipe[apps::alfred]
   recipe[apps::chrome]
   recipe[apps::dropbox]
@@ -25,5 +26,12 @@ default_attributes(
       par
       unison
     ]
+  }
+)
+
+default_attributes(
+  "one_password" => {
+    "version" => "3.8.12",
+    "checksum" => "9e7ad33a876ce9a5d1a63438d95619ca0e3240af"
   }
 )
